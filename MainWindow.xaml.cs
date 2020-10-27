5e5b3675ed1216a19bed0f21d8d691a585086f35
Сергей Захаров <sergei86zahar@mail.ru>
2020-10-27 13:49:14 +0700

    1

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,14 +15,15 @@ using System.Windows.Shapes;
 
 namespace ICWconfig
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void StartAttestation_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
