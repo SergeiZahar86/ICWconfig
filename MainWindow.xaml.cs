4d2a35b31be18680117035a82304e18f3a239cc6
Сергей Захаров <sergei86zahar@mail.ru>
2020-10-29 16:51:50 +0700

    1

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -34,9 +34,10 @@ namespace ICWconfig
 
 
 
-
             InitializeComponent();
+            VideoLeft.LoadedBehavior = MediaState.Play;
             VideoLeft.Source = new Uri("rtsp://10.90.90.5/test.mp4");
+            //VideoLeft.Source = new Uri("http://hubblesource.stsci.edu/sources/video/clips/details/images/hst_1.mpg");
             VideoLeft.Play();
         }
         private void StartAttestation_Click(object sender, RoutedEventArgs e)  // начала роботы
@@ -45,13 +46,21 @@ namespace ICWconfig
         }
         private void change_Left(object sender, RoutedEventArgs e) // изменение координат (Слева)
         {
+            string X0 = TextBox_X_0_left.Text;
+
 
         }
         private void change_Right(object sender, RoutedEventArgs e) // изменение координат (Справа)
         {
 
         }
+        private void Connect_Right(object sender, RoutedEventArgs e) // соединение
+        {
 
+        }
+        private void Connect_Left(object sender, RoutedEventArgs e) // соединение
+        {
 
+        }
     }
 }
